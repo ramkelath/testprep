24c79c6722ba4470652ee0facad6f7fb368b22db
Ram Kelath <artemis@Rams-MacBook-Air.local>
2020-06-01 18:47:38 -0400

    Move DB functions out.

--- models/db_connection.php
+++ models/db_connection.php
@@ -1,7 +1,6 @@
 <?php
 
-function OpenDBConnection()
-   {
+function OpenDBConnection() {
    $dbhost = "127.0.0.1";
    $dbuser = "root";
    $dbpass = "linering";
@@ -11,7 +10,23 @@ function OpenDBConnection()
    return $conn;
    }
 
-function CloseDBConnection($conn)
-  {
+function CloseDBConnection($conn) {
   $conn -> close();
   }
+
+function DBInsert($query, $params) {
+   $conn = OpenDBConnection();
+
+   if ($conn->connect_error) {
+    die("Connection error : " . $conn->connect_error);
+   }
+   $stmt = $conn->prepare($query);
+   /* bind parameters for markers */
+   if ($stmt) {
+      $types = str_repeat("s", count($params)); 
+      $stmt->bind_param($types, ...$params);
+      /* execute query */
+      $stmt->execute();
+   }
+   CloseDBConnection($conn);
+}
--- models/saveanswer.php
+++ models/saveanswer.php
@@ -2,11 +2,7 @@
 
 include 'db_connection.php';
 
-$conn = OpenDBConnection();
 
-if ($conn->connect_error) {
- die("Connection error : " . $conn->connect_error);
-}
 
 $insert_string = "INSERT INTO answer (answer_id, test_id, user_id, question_id, ";
 $insert_string .= " response, correct) values (?, ?, ?, ?, ?, ?) ";
@@ -17,15 +13,8 @@ $test_id = 1;
 $question_id = 1;
 $response = $_POST['questionx'];
 $correct = True;
-$stmt = $conn->prepare($insert_string);
-/* bind parameters for markers */
-if ($stmt) {
-    $stmt->bind_param("ssssss", $answer_id, $test_id, $user_id, $question_id, $response, $correct);
 
-    /* execute query */
-    $stmt->execute();
-}
-
-CloseDBConnection($conn);
+$values = array($answer_id, $test_id, $user_id, $question_id, $response, $correct);
+$saved = DBInsert($insert_string, $values);
 
 ?>
