961c1b4754329b3d85edf676134ba04dbf2c4a89
Ram Kelath <artemis@Rams-MacBook-Air.local>
2020-06-30 16:16:38 -0400

    Build correct unique key for answer table.

--- config/dbDDL.sql
+++ config/dbDDL.sql
@@ -46,13 +46,11 @@ CREATE TABLE `question` (
 
 # DDL for table correct_answer
 # -------------------------------------------------------------
-CREATE TABLE `testprep`.`answer` (
-  `answer_id` INT NOT NULL,
-  `user_id` INT NULL,
-  `test_id` INT NULL,
-  `question_id` INT NULL,
-  `response` VARCHAR(450) NULL,
-  `correct` TINYINT NULL,
-  PRIMARY KEY (`answer_id`),
-  UNIQUE KEY `test_question_id` (`test_id`,'question_id')
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE `answer` (
+  `user_id` int DEFAULT NULL,
+  `test_id` int DEFAULT NULL,
+  `question_id` int DEFAULT NULL,
+  `response` varchar(450) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `correct` tinyint DEFAULT NULL,
+  UNIQUE KEY `user_test_question` (`user_id`,`test_id`,`question_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
