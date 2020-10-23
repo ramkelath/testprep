02530520a0774490009205db27bf8fa639f69846
Ram Kelath <artemis@Rams-MacBook-Air.local>
2020-10-23 16:40:57 -0400

    Change exam composition.

--- models/db_gateway.php
+++ models/db_gateway.php
@@ -10,8 +10,35 @@ class Gateway {
     public function exam() {
         $select_query =  "SELECT parent_question_id, intro_text,  question_id, question_text, correct_answer, type,
                           wrong_answer_1, wrong_answer_2, wrong_answer_3, wrong_answer_4, wrong_answer_5, area, group_code
-                          FROM  question
-                          WHERE question_id BETWEEN 225 AND 235
+                          FROM 
+                          (
+                            SELECT * FROM 
+                            (
+                            (SELECT * FROM question 
+                            WHERE group_code = 'Cntl'
+                            LIMIT 100)
+                            UNION
+                            (SELECT * FROM question 
+                            WHERE COALESCE(group_code, 'Close') = 'Close'
+                            LIMIT 28
+                            ) 
+                            UNION
+                            (SELECT * FROM question 
+                            WHERE group_code = 'Exec'
+                            LIMIT 124) 
+                            UNION
+                            (SELECT * FROM question 
+                            WHERE group_code = 'Plan'
+                            LIMIT 96)
+                            UNION
+                            (SELECT * FROM question 
+                            WHERE area = 'Init'
+                            LIMIT 52)
+                            ) e
+                            WHERE e.question_id NOT IN
+                            (SELECT question_id FROM answer WHERE user_id = 1)
+                            ORDER BY RAND()
+                          LIMIT 200) source
                           ORDER BY CASE WHEN parent_question_id = 0 THEN question_id ELSE parent_question_id END, question_id";
         $result = DBSelect($select_query);
         return $result;
