8c647090a3d3e6b4a576a320981f313a9c421279
Ram Kelath <artemis@Rams-MacBook-Air.local>
2021-05-28 12:21:14 -0400

    Add in exampage.

---
+++ views/exampage.php
@@ -0,0 +1,34 @@
+
+<h1 class="text-center">
+      PMP Exam Preparation
+</h1>
+<br>
+<br>
+<br>
+
+<div style="width:80%; margin:auto">
+<!---
+<div class="text-left ml-5">
+<ul class="breadcrumb" style="font-size:large">
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Reports</a></li>
+</ul>
+</div>
+<br/><br/>
+---->
+<div class="text-left">
+
+<?php
+echo($exam->intro_page_text);
+?>
+<br><br><br>
+
+<a  onclick="window.open('/testprep/testcontroller.php', 
+                         'newwindow', 
+                         'width=700,height=500',
+                         'toolbar=no, location=no'); 
+              return false;" target="_blank" class="btn btn-primary">Start the test</a>
+
+
+</div>
+</div>
